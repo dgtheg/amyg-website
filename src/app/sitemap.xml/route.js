85351d75be70df29e12b074dc67e02e1b2edd68e
Daniel Gagnidze <daanielgagnidze@gmail.com>
2025-06-27 22:46:27 +0300

    Fix trailing slashes in sitemap

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -3,7 +3,7 @@
 export async function GET() {
   const baseUrl = 'https://amyg-website.vercel.app';
 
-  const staticRoutes = ['', '/about', '/projects', '/contact'];
+  const staticRoutes = ['/', '/about/', '/projects/', '/contact/'];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
