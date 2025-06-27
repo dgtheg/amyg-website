ffaf497a52389beadbc4366bc79316d4d2bc5d68
Daniel Gagnidze <daanielgagnidze@gmail.com>
2025-06-27 21:45:51 +0300

    Add dynamic sitemap route

--- src/app/sitemap/route.ts
+++ src/app/sitemap/route.ts
@@ -1,4 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
+// src/app/sitemap.xml/route.ts
+
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://amyg-website.vercel.app/</loc>
@@ -20,4 +23,12 @@
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
-</urlset>
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+      "Cache-Control": "max-age=0, s-maxage=86400", // cache for 1 day
+    },
+  });
+}
